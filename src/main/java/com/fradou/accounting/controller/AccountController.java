e544c1a5fe96416ec64c1ec992922c1090bc6e56
afradet <alex.fradet.1@gmail.com>
2018-03-22 11:15:21 +0100

    Switch from gradle to maven. Add first methods. Add custom port.

--- src/main/java/com/fradou/accounting/controller/AccountController.java
+++ src/main/java/com/fradou/accounting/controller/AccountController.java
@@ -1,13 +1,28 @@
 package com.fradou.accounting.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.fradou.accounting.model.AccountService;
+import com.fradou.accounting.model.AccountEntry;
+import com.fradou.accounting.model.AccountEntryRepository;
 
-@RequestMapping("/account")
+@RestController
 public class AccountController {
 
 	@Autowired
-	AccountService service;
+	AccountEntryRepository dao;
+
+	@RequestMapping("/entry/{id}")
+	public AccountEntry getAccountEntry(@PathVariable("id") int id) {
+		return dao.findById(id).get();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/entry")
+	public int createAccountEntry(@RequestBody AccountEntry entry) {
+		return dao.save(entry).getId();
+	}
 }
